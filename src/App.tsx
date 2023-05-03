9ea79eb17ea64ea247047727bd97f479744d094f
Dani <danial@Danis-MacBook-Pro.local>
2023-05-04 02:16:14 +0330

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import AddForm from "./AddForm/AddForm";
+import AddForm from "./components/AddForm/AddForm";
+import Header from "./components/Header/Header";
 
 function App() {
   const [toDoList, setToDoList] = useState<
@@ -16,7 +17,7 @@ function App() {
   return (
     <main className="w-screen h-screen  flex items-center justify-center">
       <div className="rounded-lg bg-slate-100 px-2 min-w-[300px] text-center h-[90vh] relative">
-        <h3 className="py-3">لیست کارها</h3>
+        <Header />
         <hr />
         {toDoList.map((item) => (
           <p key={item.id}>{item.title}</p>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h3 className="py-3">لیست کارها</h3>;
+}
