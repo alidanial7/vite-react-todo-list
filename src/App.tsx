6b7ba79d668678553fbd154688cad5befc3cd6d6
Dani <danial@Danis-MacBook-Pro.local>
2023-05-04 02:14:52 +0330

    add add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import AddForm from "./AddForm/AddForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDoList, setToDoList] = useState<
+    { id: number; title: string; completed: boolean }[]
+  >([]);
+
+  function handleAddToToDoList(title: string) {
+    setToDoList((prev) => [
+      ...prev,
+      { id: Date.now(), title, completed: false },
+    ]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-screen h-screen  flex items-center justify-center">
+      <div className="rounded-lg bg-slate-100 px-2 min-w-[300px] text-center h-[90vh] relative">
+        <h3 className="py-3">لیست کارها</h3>
+        <hr />
+        {toDoList.map((item) => (
+          <p key={item.id}>{item.title}</p>
+        ))}
+        <AddForm onAddToDoList={handleAddToToDoList} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AddForm/AddForm.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+type Props = {
+  onAddToDoList: (title: string) => void;
+};
+
+export default function AddForm({ onAddToDoList }: Props) {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  //TODO: set focus to input
+  //TODO: add validation
+  function handleAddToList(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onAddToDoList(inputValue);
+    setInputValue("");
+  }
+
+  return (
+    <form
+      onSubmit={(e) => handleAddToList(e)}
+      className="absolute bottom-0 left-0 right-0 w-full flex gap-2 px-2 flex-col mb-2"
+    >
+      <input
+        type="text"
+        placeholder="عنوان کار جدید"
+        className="w-full rounded-xl outline-none border-slate-300 border-2 p-2"
+        onChange={(event) => setInputValue(event.target.value)}
+        value={inputValue}
+      />
+      <button
+        className="bg-blue-500 text-white rounded-xl w-full p-2 disabled:bg-slate-200 disabled:text-slate-400"
+        disabled={!inputValue}
+        type="submit"
+      >
+        اضافه کردن
+      </button>
+    </form>
+  );
+}
